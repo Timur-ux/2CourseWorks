f0bf4d535f7e91e0d3c26891ef90448fba2bfa81
Timur-ux <timur.buchkin@mail.ru>
2023-09-17 12:46:00 +0300

    TODO: child parent ordering

--- OS/Lab 1/Unix/main.cpp
+++ OS/Lab 1/Unix/main.cpp
@@ -16,39 +16,38 @@ int main() {
     
     string fileName;
     cout << "Input subprogram name: ";
-    cin >> fileName;
+    // cin >> fileName;
+    fileName = "subprogram";
 
     cout << "Input \"num1 num2 ...<endline>\" as command" << endl;
     cout << "Input \"Exit\" for exit" << endl;
-
     int pid = fork();
     if( pid < 0 ) {
         cerr << "Error: fork creating error" << endl;
     } else if( pid == 0 )  { // Child process
         close(fd1[WRITE]);
         close(fd2[READ]);
-        char * argv[] = {"subprogram", (char *)to_string(fd2[WRITE]).c_str(),
+        char * argv[] = {(char *)fileName.c_str(), (char *)to_string(fd2[WRITE]).c_str(),
                         (char*)to_string(fd1[READ]).c_str()};
         execv(argv[0], argv);
-    }
-    // main process next
-    close(fd1[READ]);
-    close(fd2[WRITE]);
-
-    while( true ) {
-        string command;
-        getline(cin, command);
-
-        if(command == "Exit") {
-            break;
+    } else {
+        close(fd1[READ]);
+        close(fd2[WRITE]);
+        while( true ) {
+            string command;
+            getline(cin, command);
+            int len = command.size();
+            fflush(NULL);
+            if( write(fd1[WRITE], &len, sizeof(int)) != sizeof(int) ||
+            write(fd1[WRITE], command.c_str(), len) != len) {
+                cout << "Error: write to pipe failed" << endl;
+            }
+            if(command == "Exit")
+                break;
         }
 
-        if( write(fd1[WRITE], &command, sizeof(string)) != sizeof(string) ) {
-            cerr << "Error: write command failed" << endl;
-        }
+        close(fd1[WRITE]);
+        close(fd2[READ]);
     }
-
-    close(fd1[WRITE]);
-    close(fd2[READ]);
     return 0;
 }
--- OS/Lab 1/Unix/subprogram.cpp
+++ OS/Lab 1/Unix/subprogram.cpp
@@ -1,16 +1,65 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <vector>
+#include <cstddef>
 
 using namespace std;
 
 enum PIPES {READ, WRITE};
 
-int main(int argc, char * argw[]) {
+int calc(const char * s, int len) {
+    int result = 0, temp = 0;
+    for (size_t i = 0; i < len; ++i)
+    {
+        if(s[i] == ' ') {
+            result += temp;
+            temp = 0;
+            continue;
+        }
+        temp = temp*10 + (s[i]-'0');
+    }
+    result += temp;
+    return result;
+}
+
+bool isExit(const char * s) {
+    const char * exitCommand = "Exit";
+    for(int i = 0; i < 4; ++i) {
+        if(s[i] != exitCommand[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char * argv[]) {
     if(argc < 3) {
         cerr << "Error: no input/output file descritors given" << endl;
     }
     
-    int inputFD = atoi(argv[1]);
-    int outputFD = atoi(argv[2]);
+    int writeFD = atoi(argv[1]);
+    int readFD = atoi(argv[2]);
+    cout << "SP is running..." << endl;
+    FILE * outFile = fopen("Output.txt", "w");
+    // int outStore = dup(fileno(stdout));
+    // if(dup2(fileno(stdin), readFD) == -1 || 0) {
+    //     dup2(fileno(outFile), fileno(stdout)) == -1) {
+    //         cerr << "Error: dup2 failed" << endl;
+    // }
+    while(true) {
+        dup2(readFD, fileno(stdin));
+        dup2(fileno(outFile), fileno(stdout));
+        int len;
+        char command[256];     
+        if(read(readFD, &len, sizeof(int)) != sizeof(int) || read(readFD, command, len) != len ||
+            isExit(command)) {
+            break;
+        }
+        int result = calc(command, len);
+        cout << result << '\n';
+    }
+    fclose(outFile);
+    close(writeFD);
+    close(readFD);
 }

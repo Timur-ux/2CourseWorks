7d08699dd99a0ba49834da10ae917a9f7116faf1
TABuchkin <tabuchkin@mai.education>
2023-10-15 11:56:57 +0300

    Lab 3: validators

--- OOP/Lab 3/include/Romb.hpp
+++ OOP/Lab 3/include/Romb.hpp
@@ -6,8 +6,6 @@ namespace geometry {
 
     class Romb : public Figure {
     private:
-        virtual void assertPoints(const std::vector<Point> & _points) const override;
-        virtual std::vector<Point> unificatePoints(std::vector<Point> _points) override;
         virtual Point calcGeometryCenter(const std::vector<Point> & _points) const override;
         virtual double calcSquare(const std::vector<Point> & _points) const override;
     public:
@@ -16,11 +14,6 @@ namespace geometry {
         Romb(std::vector<Point> && _points);
         Romb(const Romb & other);
         Romb(Romb && other) noexcept;
-
-        virtual Figure & operator=(const Figure & rhs) override;
-        virtual Figure & operator=(Figure && rhs) override;
-
-        virtual bool operator==(const Figure & rhs) const override;
     };
 
 };
--- OOP/Lab 3/src/Trapezoid.cpp
+++ OOP/Lab 3/src/Trapezoid.cpp
@@ -2,37 +2,22 @@
 
 using namespace geometry;
 
-void Trapezoid::assertPoints(const std::vector<Point> & _points) const {
-    if(_points.size() != 4) {
-        throw new std::invalid_argument("Incorrect points for Trapezoid");
-    }
-
-    Line line1(_points[0], _points[1]);
-    Line line2(_points[2], _points[3]);
-    if(not (line1 || line2)) {
-        throw new std::invalid_argument("Trapezoid must have minimum 1 pair of parallel sides");
-    }
-}
-
-std::vector<Point> Trapezoid::unificatePoints(std::vector<Point> _points) {
-    return Figure::unificatePoints(_points);
-}
-
-Trapezoid::Trapezoid() : Figure(4, "Trapezoid") {
-    square = 0;
-    geometryCenter = Point(0, 0);
-}
+Trapezoid::Trapezoid() : Figure(4, "Trapezoid", *(new TrapezoidValidator)) {}
 
 Trapezoid::Trapezoid(std::vector<Point> &_points) : Trapezoid() {
     points = unificatePoints(_points);
-    geometryCenter = calcGeometryCenter(_points);
-    square = calcSquare(_points);
+    validator.validate(points);
+
+    geometryCenter = calcGeometryCenter(points);
+    square = calcSquare(points);
 }
 
 Trapezoid::Trapezoid(std::vector<Point> &&_points) : Trapezoid() {
     points = unificatePoints(_points);
-    geometryCenter = calcGeometryCenter(_points);
-    square = calcSquare(_points);
+    validator.validate(points);
+
+    geometryCenter = calcGeometryCenter(points);
+    square = calcSquare(points);
 }
 
 Trapezoid::Trapezoid(const Trapezoid &other) : Trapezoid() {
@@ -45,24 +30,13 @@ Trapezoid::Trapezoid(Trapezoid && other) noexcept : Trapezoid() {
     points = other.getPoints();
     geometryCenter = other.getCenter();
     square = double(other);
-    
-    delete &other;
 }
 
 Point Trapezoid::calcGeometryCenter(const std::vector<Point> & _points) const {
-    Point diff13 = _points[2] - _points[0];
-    double k1 = diff13.getY() / diff13.getX();
+    Line diag1(_points[0], _points[2]);
+    Line diag2(_points[1], _points[3]);
 
-    Point diff24 =  _points[3] - _points[1];
-    double k2 = diff24.getY() / diff24.getX();
-
-    double b1 = _points[0].getY() - _points[0].getX() * k1;
-    double b2 = _points[1].getY() - _points[1].getX() * k2;
-
-    double crossX = (b1 - b2) / (k2 - k1);
-    double crossY = k1 * crossX + b1;
-
-    return Point(decart(crossX, crossY));
+    return calcCross(diag1, diag2);
 }
 
 double Trapezoid::calcSquare(const std::vector<Point> & _points) const {
@@ -71,28 +45,4 @@ double Trapezoid::calcSquare(const std::vector<Point> & _points) const {
     double h = abs(_points[0] - _points[2]).getY();
 
     return (a + b) * h / 2; 
-}
-
-Figure &Trapezoid::operator=(const Figure &rhs) {
-    points = unificatePoints(rhs.getPoints());
-    square = double(rhs);
-    geometryCenter = rhs.getCenter();
-
-    return *this; 
-}
-
-Figure &Trapezoid::operator=(Figure &&rhs) {
-    points = unificatePoints(rhs.getPoints());
-    square = double(rhs);
-    geometryCenter = rhs.getCenter();
-    
-    delete &rhs;
-    return *this;
-}
-
-bool Trapezoid::operator==(const Figure &rhs) const {
-    std::vector<Point> rhsPoints = rhs.getPoints();
-    assertPoints(rhsPoints);
-
-    return double(*this) == double(rhs);
 }

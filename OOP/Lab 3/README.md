d887f7439094ca071e833e5b9666d3f122a64750
TABuchkin <tabuchkin@mai.education>
2023-11-13 15:56:06 +0300

    delete files from OOP and FuncLogProg

--- OOP/Lab 3/README.md
+++
@@ -1,5 +0,0 @@
-# Calculating variant number
-
-I'am 2 in order list, and we have 20 variants, so:
-
-MyVariant = 2 % 20 + 1 = 3;
--- OOP/Lab 4/test/main_tests.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char * argw[]) {
-    testing::InitGoogleTest(&argc, argw);
-    return RUN_ALL_TESTS();
-}
